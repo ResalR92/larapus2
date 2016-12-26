4a84101d3efe522220693654d2f91231d493671b
ResalR92 <resalramdahadi92@gmail.com>
2016-12-26 22:17:39 +0700

    Konfigurasi email->from

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'admin@resalr.com',
+        'name' => 'Admin ResalR',
     ],
 
     /*
