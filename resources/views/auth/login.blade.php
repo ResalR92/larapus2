34fd32efba5eca48f11e6d785bd9d40c52d28280
ResalR92 <resalramdahadi92@gmail.com>
2016-12-26 22:40:54 +0700

    mengubah login->laravelcollective

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,59 +7,44 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+                    {!! Form::open(['url'=>'login', 'class'=>'form-horizontal']) !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        {!! Form::label('email', 'Alamat Email', ['class'=>'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+                            {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+                        </div>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        {!! Form::label('password', 'Password', ['class'=>'col-md-4 control-label']) !!}
+                        <div class="col-md-6">
+                            {!! Form::password('password', ['class'=>'form-control']) !!}
+                            {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
+                        </div>
+                    </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    {!! Form::checkbox('remember')!!} Ingat saya
+                                </label>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-sign-in"></i> Login
+                            </button>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            <a class="btn btn-link" href="{{ url('/password/reset') }}">Lupa password</a>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
