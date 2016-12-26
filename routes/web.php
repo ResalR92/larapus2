3aaaac08f4785af27836eeed1f32c3c419fa8eee
ResalR92 <resalramdahadi92@gmail.com>
2016-12-26 11:57:03 +0700

    akses model #3-> App\Post::where('title','like','%mahir%')

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/about', function(){
 });
 
 Route::get('/testModel',function(){
-	$query = App\Post::find(1);
+	$query = App\Post::where('title','like','%mahir%')->get();
 	return $query;
 });
