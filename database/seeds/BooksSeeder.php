2b8fb92a5ea3f439834ad0b8f8b8afada0954b96
ResalR92 <resalramdahadi92@gmail.com>
2016-12-29 19:23:51 +0700

    Membuat seeder->books,author

--- database/seeds/BooksSeeder.php
+++ database/seeds/BooksSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Author;
+use App\Book;
 
 class BooksSeeder extends Seeder
 {
@@ -11,6 +13,15 @@ class BooksSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Sampel penulis
+        $author1 = Author::create(['name'=>'Mohammad Fauzil Adhim']);
+        $author2 = Author::create(['name'=>'Salim A. Fillah']);
+        $author3 = Author::create(['name'=>'Aam Amiruddin']);
+
+        //Sample buku
+        $book1 = Book::create(['title'=>'Kupinang Engkau dengan Hamdalah','amount'=>3,'author_id'=>$author1->id]);
+        $book2 = Book::create(['title'=>'Jalan Cinta Para Pejuang','amount'=>2,'author_id'=>$author2->id]);
+        $book3 = Book::create(['title'=>'Membingkai Surga dalam Rumah Tangga','amount'=>4,'author_id'=>$author3->id]);
+        $book4 = Book::create(['title'=>'Cinta & Seks Rumah Tangga Muslim','amount'=>3,'author_id'=>$author3->id]);
     }
 }
