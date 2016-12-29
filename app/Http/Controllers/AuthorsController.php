c7da8692e84f4f429af609619e5418d5ba94b0f8
ResalR92 <resalramdahadi92@gmail.com>
2016-12-29 20:40:13 +0700

    membuat author->view->index

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -13,7 +13,7 @@ class AuthorsController extends Controller
      */
     public function index()
     {
-        //
+        return view('authors.index');
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel='stylesheet' type='text/css'> 
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel='stylesheet' type='text/css'>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/jquery.dataTables.css') }}" rel="stylesheet">
@@ -51,6 +51,7 @@
                     <ul class="nav navbar-nav">
                         @if(Auth::check())
                             <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                            <li><a href="{{ route('authors.index') }}">Penulis</a></li>
                         @endif
                     </ul>
 
