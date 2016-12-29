c59fb9ba51689fc95c2e808d5a7506bdb9da467e
ResalR92 <resalramdahadi92@gmail.com>
2016-12-29 19:12:48 +0700

    Membuat relasi one-to-many

--- app/Book.php
+++ app/Book.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = ['title','author_id','amount'];
+
+    public function author()
+    {
+      return $this->belongsTo('App\Author');
+    }
 }
