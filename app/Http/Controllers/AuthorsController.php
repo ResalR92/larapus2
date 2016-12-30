9b0c358a4cd210a7ee86b7463d857380a3e95d3c
ResalR92 <resalramdahadi92@gmail.com>
2016-12-30 13:47:14 +0700

    form model binding -> update -> author

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -19,10 +19,16 @@ class AuthorsController extends Controller
     {
         if($request->ajax()){
           $authors = Author::select(['id','name']);
-          return Datatables::of($authors)->make(true);
+          return Datatables::of($authors)
+                ->addColumn('action',function($author){
+                  return view('datatable._action',[
+                    'edit_url' => route('authors.edit', $author->id),
+                  ]);
+                })->make(true);
         }
         $html = $htmlBuilder
-              ->addColumn(['data'=>'name','name'=>'name','title'=>'Nama']);
+              ->addColumn(['data'=>'name','name'=>'name','title'=>'Nama'])
+              ->addColumn(['data'=>'action','name'=>'action','title'=>'','orderable'=>false,'searchable'=>false]);
 
         return view('authors.index')->with(compact('html'));
     }
@@ -75,7 +81,8 @@ class AuthorsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $author = Author::find($id);
+        return view('authors.edit')->with(compact('author'));
     }
 
     /**
@@ -87,7 +94,15 @@ class AuthorsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,['name'=>'required|unique:authors,name,'.$id]);
+        $author = Author::find($id);
+        $author->update($request->only('name'));
+        Session::flash('flash_notification',[
+          'level' => 'success',
+          'message' => "Berhasil menyimpan $author->name"
+        ]);
+
+        return redirect()->route('authors.index');
     }
 
     /**
---
+++ resources/views/datatable/_action.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ $edit_url }}" class="btn btn-warning">Ubah</a>
