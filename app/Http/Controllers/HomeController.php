3b776b643f66cf7d7392c3262c693f54698ccac9
ResalR92 <resalramdahadi92@gmail.com>
2016-12-30 21:56:48 +0700

    Membuat dashboard -> laratrust -> admin,member

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laratrust\LaratrustFacade as Laratrust;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Laratrust::hasRole('admin')) return $this->adminDashboard();
+        if(Laratrust::hasRole('member')) return $this->memberDashboard();
+    }
+
+    protected function adminDashboard()
+    {
+        return view('dashboard.admin');
+    }
+
+    protected function memberDashboard()
+    {
+        return view('dashboard.member');
     }
 }
