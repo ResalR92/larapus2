0ca8cd6d6459f7a10878f99932c4083e110fde21
ResalR92 <resalramdahadi92@gmail.com>
2016-12-30 20:29:12 +0700

    borrowLog -> booksSeeder -> sample

--- database/seeds/BooksSeeder.php
+++ database/seeds/BooksSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Author;
 use App\Book;
+use App\BorrowLog;
+use App\User;
 
 class BooksSeeder extends Seeder
 {
@@ -23,5 +25,11 @@ class BooksSeeder extends Seeder
         $book2 = Book::create(['title'=>'Jalan Cinta Para Pejuang','amount'=>2,'author_id'=>$author2->id]);
         $book3 = Book::create(['title'=>'Membingkai Surga dalam Rumah Tangga','amount'=>4,'author_id'=>$author3->id]);
         $book4 = Book::create(['title'=>'Cinta & Seks Rumah Tangga Muslim','amount'=>3,'author_id'=>$author3->id]);
+
+        // Sample peminjaman buku
+        $member = User::where('email', 'member@gmail.com')->first();
+        BorrowLog::create(['user_id' => $member->id, 'book_id'=>$book1->id, 'is_returned' => 0]);
+        BorrowLog::create(['user_id' => $member->id, 'book_id'=>$book2->id, 'is_returned' => 0]);
+        BorrowLog::create(['user_id' => $member->id, 'book_id'=>$book3->id, 'is_returned' => 1]);
     }
 }
