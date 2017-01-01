183b01b1f219214aaadfc4cda63fb0a9b6b24b72
ResalR92 <resalramdahadi92@gmail.com>
2017-01-02 00:33:43 +0700

    Membatasi akses -> belum verifikasi -> middleware

---
+++ app/Http/Middleware/UserShouldVerified.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class UserShouldVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        if(Auth::check() && !Auth::user()->is_verified) {
+            Auth::logout();
+
+            Session::flash('flash_notification',[
+                'level' => 'warning',
+                'message' => "Akun Anda belum aktif. Silahkan klik pada link aktivasi yang telah kami kirim"
+            ]);
+            return redirect('/login');
+        }
+        return $response;
+    }
+}
