63ac06876e94be52ec45d9ab14d255bff31090fe
ResalR92 <resalramdahadi92@gmail.com>
2017-01-02 20:37:12 +0700

    Membuat sumbu array -> x->author dan y->buku->count()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Laratrust\LaratrustFacade as Laratrust;
 use Illuminate\Support\Facades\Auth;
+use App\Author;
 
 class HomeController extends Controller
 {
@@ -31,7 +32,14 @@ class HomeController extends Controller
 
     protected function adminDashboard()
     {
-        return view('dashboard.admin');
+        $authors = [];
+        $books = [];
+        foreach(Author::all() as $author) {
+            array_push($authors, $author->name);
+            array_push($books, $books->count());
+        }
+
+        return view('dashboard.admin',compact('authors','books'));
     }
 
     protected function memberDashboard()
